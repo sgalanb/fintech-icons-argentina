76ee14b3a2f2937aa0d0aa63151af1fbbf12caab
sgalanb <73245438+sgalanb@users.noreply.github.com>
2024-08-30 14:40:21 -0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Header from '@/app/components/header'
 import IconList from '@/app/components/icon-list'
+import { Suspense } from 'react'
 
 export default function Home() {
   return (
     <main className="mb-6 flex w-full max-w-3xl flex-col items-center justify-center gap-3">
       <Header />
-      <IconList />
+      <Suspense>
+        <IconList />
+      </Suspense>
     </main>
   )
 }
